036afdc050a26fae8bbcd7b2cf1544ae3156ed8d
s1120388-cpy <s1120388@dmhs.kh.edu.tw>
2025-10-02 06:23:48 +0000

    作業4

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using namespace std;
-// 華視攝氏轉換
+
 int main() {
-    double fahrenheit;
-    double celsius;
-    cout << "請輸入華氏溫度:\n";
-    cin >> fahrenheit;
-    celsius = (fahrenheit - 32) / 9 * 5;
-    cout << "攝氏溫度是:" << celsius;
+    int sum = 0;
+    for (int i = 1; i <= 50; i++) {
+        if ((i % 7) == 0) cout << i << " ";
+    }
     return 0;
 }
