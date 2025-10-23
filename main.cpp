e02f3efbdd29c30b9fae08c5973db276840c4570
s1120388-cpy <s1120388@dmhs.kh.edu.tw>
2025-10-23 14:05:16 +0800

    作業6

--- main.cpp
+++ main.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // 九九乘法表（使用 while 迴圈）
-    int i = 1;
-    int j;
-
-    cout << "=== 九九乘法表 ===" << endl;
-    while (i <= 9) {
-        j = 1;
-        while (j <= 9) {
-            cout << i << " * " << j << " = " << i * j << "\t";
-            j += 1;
+    int score[10] = {85, 90, 60, 92, 100, 76, 50, 89, 84, 40};
+    int gradeCount[5] = {0};
+    
+    for (int i = 0; i < 10; ++i) {
+        if (score[i] > 90) {
+            gradeCount[0]++;
+        } else if (score[i] >= 80) {
+            gradeCount[1]++;
+        } else if (score[i] >= 70) {
+            gradeCount[2]++;
+        } else if (score[i] >= 60) {
+            gradeCount[3]++;
+        } else {
+            gradeCount[4]++;
         }
-        cout << endl;
-        i += 1;
     }
-
-    cout << endl;
-
-    // 星號直角三角形（使用 for 迴圈）
-    cout << "=== 星號三角形 ===" << endl;
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    
+    cout << "A等人數: " << gradeCount[0] << endl;
+    cout << "B等人數: " << gradeCount[1] << endl;
+    cout << "C等人數: " << gradeCount[2] << endl;
+    cout << "D等人數: " << gradeCount[3] << endl;
+    cout << "F等人數: " << gradeCount[4] << endl;
 
     return 0;
 }
