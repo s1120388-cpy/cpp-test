d4f2a480e192b33a54c1d6796264493769bbdabc
s1120388-cpy <s1120388@dmhs.kh.edu.tw>
2025-11-06 14:29:43 +0800

    作業7

--- main.cpp
+++ main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    int score[10] = {85, 90, 60, 92, 100, 76, 50, 89, 84, 40};
-    int gradeCount[5] = {0};
-    
-    for (int i = 0; i < 10; ++i) {
-        if (score[i] > 90) {
-            gradeCount[0]++;
-        } else if (score[i] >= 80) {
-            gradeCount[1]++;
-        } else if (score[i] >= 70) {
-            gradeCount[2]++;
-        } else if (score[i] >= 60) {
-            gradeCount[3]++;
-        } else {
-            gradeCount[4]++;
-        }
+    int stack[5];
+    int top = -1;
+
+    top++;
+    stack[top] = 10;
+    top++;
+    stack[top] = 20;
+    top++;
+    stack[top] = 30;
+
+    cout << "目前堆疊內容: " << endl;
+    for (int i = 0; i <= top; i++) {
+        cout << stack[i] << " ";
     }
-    
-    cout << "A等人數: " << gradeCount[0] << endl;
-    cout << "B等人數: " << gradeCount[1] << endl;
-    cout << "C等人數: " << gradeCount[2] << endl;
-    cout << "D等人數: " << gradeCount[3] << endl;
-    cout << "F等人數: " << gradeCount[4] << endl;
+    cout << endl;
+
+    if (top >= 0) {
+        cout << "彈出元素: " << stack[top] << endl;
+        top--;
+    } else {
+        cout << "堆疊是空的，不能彈出!" << endl;
+    }
+
+    cout << "彈出後堆疊內容: " << endl;
+    for (int i = 0; i <= top; i++) {
+        cout << stack[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
